8d4b6eeea0223dbdabfd3d490d3af0c742153bd2
张逸菲 <zhangzhangyifei@zhangyifeideMacBook-Air.local>
2017-04-08 16:04:08 +1000

    finished 2. the hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
