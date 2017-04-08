1dc348c2ba95dcd1509ad9453c4b8eaccff8f771
张逸菲 <zhangzhangyifei@zhangyifeideMacBook-Air.local>
2017-04-08 19:48:02 +1000

    finished 3. Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,5 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
-
-export class AppComponent {
-  title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
-
-}
-
 export class Hero {
   id: number;
   name: string;
@@ -34,4 +18,25 @@ const HEROES: Hero[] = [
   { id: 20, name: 'Tornado' }
 ];
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  selectedHero: Hero;
+
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
 
