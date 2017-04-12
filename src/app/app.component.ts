84165e8c315e3c1add40c2e99f2c4f7667c1bf5e
张逸菲 <zhangzhangyifei@zhangyifeideMacBook-Air.local>
2017-04-12 21:56:50 +1000

    finished 6.Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,14 @@
+
 import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
-
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
+
 })
 
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { };
-
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
+    title = 'Tour of Heroes';
+    
 }
-
